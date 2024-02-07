564f76259f64d3a03f0666e9e3e4ce855ce29478
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-07 12:31:06 +0100

    Added signup form handling
    New user created on the signup controller after form verifications.

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,63 @@ const { body, validationResult } = require("express-validator");
 const asyncHandler = require('express-async-handler');
 require("dotenv").config()
 
-exports.signup = asyncHandler(async(req,res,next));
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+exports.signup = [
+    body("username", "Must be between 4 and 16 characters")
+        .trim()
+        .isLength({min:4, max:16})
+        .escape(),
+    body("password", "Must be at least 8 characters long")
+        .trim()
+        .isLength({min:8}),
+    body("password_verif").custom((value, { req }) => {
+        if (value !== req.body.password) throw new Error('Passwords do not match')
+        return true;
+    }),
+    body("first_name")
+        .trim()
+        .isLength({min:2})
+        .withMessage("Must be at least 2 characters")
+        .isLength({max:16})
+        .withMessage("Must be a maximum of 12 characters")
+        .escape(),
+    body("last_name")
+        .trim()
+        .isLength({min:2})
+        .withMessage("Must be at least 2 characters")
+        .isLength({max:16})
+        .withMessage("Must be a maximum of 12 characters")
+        .escape(),
+    body("location")
+        .trim()
+        .optional(),
+
+    upload.single("profile_picture"),
+
+    asyncHandler(async(req,res,next) => {
+// Validate form
+        const errors = validationResult(req);
+        if(!errors.isEmpty) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const user = new User({
+            username: req.body.username,
+            password: hashedPassword,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            location: req.body.location,
+            profile_picture: req.file ? req.file.path : null,
+        })
+        await user.save();
+        res.status(200).json({user:user})
+
+
+
+    })]
 
 exports.login = asyncHandler(async(req,res,next));
 
