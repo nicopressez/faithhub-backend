e1ebfce994deb70d537bbc34cdbc57eafc49583e
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-08 11:51:54 +0100

    Added profile updated and delete tests

--- __tests__/profileTest.js
+++ __tests__/profileTest.js
@@ -3,11 +3,13 @@ const setup = require('./testSetup')
 const initializeMongoServer = require('./mongoSetup');
 const User = require('../models/user');
 const bcrypt = require("bcryptjs")
+const express = require('express');
+const app = express();
+const router = express.Router();
+
 
 const request = require('supertest');
-const express = require('express');
-const user = require('../models/user');
-const app = express();
+const profileController = require('../controllers/profileController');
 
 // Call setups
 setup();
@@ -15,7 +17,13 @@ initializeMongoServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/", profileRouter)
+
+// Overwrite routes so it doesn't check tokens
+app.patch('/:id/update', profileController.profile_update);
+app.delete('/:id/delete', profileController.profile_delete);
+
+// Set route
+app.use("/profile", profileRouter)
 
 // Fill database before tests
 beforeEach( async() => {
@@ -36,14 +44,37 @@ beforeEach( async() => {
 
   // Tests
 
-  test("Return user info on get route", async () => {
+  test("Return user info on get route (no token check)", async () => {
     const testuser = await User.findOne({username: "testuserdb"})
     const testuserid = testuser._id
     const response = await request(app)
-        .get(`/${testuserid}`)
+        .get(`/profile/${testuserid}`)
 
       expect(response.status).toBe(200)
       expect(response.headers["content-type"]).toMatch(/json/);
       expect(response.body.user.username).toEqual("testuserdb");
 
+  })
+
+  test("Update user info (no token check)", async() => {
+    const testuser = await User.findOne({username: "testuserdb"})
+    const testuserid = testuser._id
+    const payload = {username: "edited",
+                     first_name: "John",
+                     last_name: "Doe",
+                     location: "paris"}
+
+    const response = await request(app)
+    .patch(`/${testuserid}/update`)
+    .send(payload)
+    expect(response.status).toBe(200)
+  })
+
+  test("Delete user info (no token check)", async() => {
+    const testuser = await User.findOne({username: "testuserdb"})
+    const testuserid = testuser._id
+
+    const response = await request(app)
+    .delete(`/${testuserid}/delete`)
+    expect(response.status).toBe(200)
   })
