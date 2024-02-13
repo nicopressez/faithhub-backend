ac9d7a309f53faef077c66da2f452eacfebffb58
nicopressez <154272808+nicopressez@users.noreply.github.com>
2024-02-13 13:27:49 +0100

    Added comments controller & routes

---
+++ controllers/commentsController.js
@@ -0,0 +1,24 @@
+const Comment = require('../models/comment')
+const User = require('../models/user')
+const Post = require('../models/comment')
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require('express-async-handler');
+
+// Get all comments
+exports.all_comments = asyncHandler(async(req,res,next) => {
+
+})
+
+exports.create_comment = asyncHandler(async(req,res,next) => {
+
+})
+
+exports.update_comment = asyncHandler(async(req,res,next) => {
+
+})
+
+exports.delete_comment = asyncHandler(async(req,res,next) => {
+    
+}
+)
+
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const postsController = require('../controllers/postsController')
+const commentsController = require('../controllers/commentsController')
 
 const verifyRefreshToken = require('../utils/tokenVerif');
 
-router.get('/', function(req, res, next) {
-    res.status(200).json({message: "Post router works"});
-  });
+
+  // Update one comment
+  router.patch('/:id/comments/:commentid', verifyRefreshToken, commentsController.update_comment)
+
+  // Delete one comment
+  router.delete('/:id/comments/:commentid', verifyRefreshToken, commentsController.delete_comment)
+
+  // Create one comment
+  router.post('/:id/comments', verifyRefreshToken, commentsController.create_comment)
+
+  // Get all comments by post
+  router.get('/:id/comments', commentsController.all_comments )
 
   // Update one post
   router.patch('/:id', verifyRefreshToken, postsController.update_post)
@@ -22,4 +32,12 @@ router.get('/', function(req, res, next) {
 
   // Create post
   router.post('/', verifyRefreshToken, postsController.create_post)
+
+  // Test route
+  router.get('/', function(req, res, next) {
+    res.status(200).json({message: "Post router works"});
+  });
+
+
+
 
